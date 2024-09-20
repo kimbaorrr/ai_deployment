15e13ece782f7a231c6830e839db2075dd3d9709
Bao <nguyenkimbao.0708@gmail.com>
2024-09-21 01:34:56 +0700

    first commit

---
+++ toxic_comments/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./assets/*/*.{js, css}",
+    "./*.html",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+     
+        require('flowbite/plugin')
+  ],
+}
+
